07e87b9e0e5ce8424d960f40a62901889ab62e25
mazen <mazenshaar@hotmail.com>
2022-08-22 19:50:46 +0400

    Terrain Generation done with heightmap + perlin noise

---
+++ Assets/Triangle2.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using API;
+
+public class Triangle2 : MonoBehaviour
+{
+    Mesh mesh; // new Mesh
+    MeshRenderer meshRenderer;
+    MeshFilter meshFilter;
+    [SerializeField] Material material; //material that will be used
+    [SerializeField] float elevationFactor;
+
+    Vector3[] vertices; // vertices we gonna assign to the Mesh
+    int[] triangles; // triangles we gonna assign to the Mesh
+    public int xSize = 20; // Grid X size
+    public int zSize = 20; // Grid Z size   
+    const int rectangleIndex = 6;
+
+    void Start()
+    {
+        RandomAPI aPI = new RandomAPI();
+        aPI.Speak();
+
+        mesh = new Mesh();
+
+        meshRenderer = gameObject.AddComponent<MeshRenderer>(); //instead of doing it in the inspector
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+        GetComponent<MeshFilter>().mesh = mesh; // Assign created Mesh to the game component
+        GetComponent<MeshRenderer>().material = material; //assigning the material
+        CreateShape();
+        UpdateMesh();
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)]; // Create the vertices for the grid.
+                                                           // x and z grid vertices N are always +1 the number of squares along x and z axis,
+                                                           // so if the grid is 20x20 squares big, vertices count will be (20+1)x(20+1)
+
+        // Create the grid using two cascading loops
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                float y = Mathf.PerlinNoise(x * elevationFactor, z * elevationFactor);// * 2f; // Variable used to make the mesh surface uneven
+                vertices[i] = new Vector3(x, y, z);
+            }
+        }
+            
+        triangles = new int[xSize * zSize * rectangleIndex]; //20 * 20 * 6 = 2400, and rectangle index is 6
+        int vert = 0; // Track vertices co-ords
+        int tris = 0; // Track triangles. Used to create new triagles on each new row
+
+        // Fill the grid with quads with cascading loops
+        for (int z = 0; z < zSize; z++) // Create quads along the z-axis
+        {
+            // Copy 1st quad along grid x-axis
+            for (int x = 0; x < xSize; x++)
+            {
+                // Create 2 new clockwise triangle to create a quad for one square of the grid
+                // Starting from 1st triangle, tris = 0, vert =0.
+                triangles[tris + 0] = vert + 0; // Starting from 0 (Grid start point)
+                triangles[tris + 1] = vert + xSize + 1; // Go 1 up along x-axis (clockwise), which equals to 20+1 points
+                triangles[tris + 2] = vert + 1; // Goto 1 along z
+                                                // 2nd triangle
+                triangles[tris + 3] = vert + 1; // Same point as triangles[2]
+                triangles[tris + 4] = vert + xSize + 1; // Same point as triangles[1]
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++; // Update co-ords to create new quads
+                tris += 6;
+            }
+
+            vert++;
+        }
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        // Assign vertices and triangles we've made to the Mesh
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals(); // Correct coordinates system for correct lighting       // we going to do this manually
+    }
+}
---
+++ Assets/TriangleClass.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleClass : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    MeshFilter meshFilter;
+
+    int mapWidth = 500;
+    int mapHeight = 100;
+
+
+    void Start()
+    {
+        meshRenderer = gameObject.AddComponent<MeshRenderer>(); //instead of doing it in the inspector
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+        /*
+        meshFilter.mesh.vertices = new Vector3[]
+        {
+            /*
+               new Vector3(-1, 0 , -1),   //0 
+               new Vector3(-1, 0 , 1), //1
+               new Vector3(1, 0 , 1),//2
+               new Vector3(1, 0 , -1),//3
+               new Vector3(-1, 0 , -1),//4
+               new Vector3(1, 0 , 1),//5
+            */ /*
+            new Vector3(-1, 0 , -1),
+            new Vector3(-1, 0 , 1),
+            new Vector3(1, 0 , 1),
+            new Vector3(1, 0 , -1),
+
+        };
+
+        meshFilter.mesh.triangles = new int[]
+{   
+        0,
+        1,
+        2,
+        2,
+        3,
+        0,
+};
+*/
+        int height = 10;
+        int width = 500;
+        
+        Vector3[] verts = new Vector3[mapWidth * mapHeight * 6];
+        Vector2[] uvs = new Vector2[mapWidth * mapHeight * 6];
+        int[] triangles = new int[mapWidth * mapHeight * 6];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+
+                int tileIndex = x + y * width;
+
+                verts[tileIndex * 6 + 0] = new Vector3(x, 0, y);
+                verts[tileIndex * 6 + 1] = new Vector3(x + 1, 0, y);
+                verts[tileIndex * 6 + 2] = new Vector3(x + 1, 0, y + 1);
+
+                verts[tileIndex * 6 + 3] = new Vector3(x, 0, y);
+                verts[tileIndex * 6 + 4] = new Vector3(x + 1, 0, y + 1);
+                verts[tileIndex * 6 + 5] = new Vector3(x, 0, y + 1);
+
+                uvs[tileIndex * 6 + 0] = new Vector2(0, 0);
+                uvs[tileIndex * 6 + 1] = new Vector2(1, 0);
+                uvs[tileIndex * 6 + 2] = new Vector2(1, 1);
+
+                uvs[tileIndex * 6 + 3] = new Vector2(0, 0);
+                uvs[tileIndex * 6 + 4] = new Vector2(1, 1);
+                uvs[tileIndex * 6 + 5] = new Vector2(0, 1);
+
+                for (int t = 0; t < 6; t++)
+                {
+                    triangles[tileIndex * 6 + t] = tileIndex * 6 + 5 - t;
+                }
+            }
+        }
+
+        meshFilter.mesh.vertices = verts;
+        meshFilter.mesh.uv = uvs;
+        meshFilter.mesh.triangles = triangles;
+       // meshFilter.mesh.RecalculateNormals();
+    }
+}
